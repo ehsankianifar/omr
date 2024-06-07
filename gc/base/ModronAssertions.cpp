6b05bb4a9c27ddad4d7a8ae3f6fe7103333d829f
Ehsan Kiani Far <ehsan.kianifar@gmail.com>
2024-06-07 18:48:56 -0400

    Comment assertion
    a

--- gc/base/ModronAssertions.cpp
+++ gc/base/ModronAssertions.cpp
@@ -21,6 +21,7 @@
  *******************************************************************************/
 
 #include <stdarg.h>
+#include <stdio.h>
 
 #include "omr.h"
 #include "ModronAssertions.h"
@@ -41,18 +42,21 @@ extern "C" {
 void
 omrGcDebugAssertionOutput(OMRPortLibrary *portLibrary, OMR_VMThread *omrVMThread, const char *format, ...)
 {
-	char buffer[ASSERTION_MESSAGE_BUFFER_SIZE];
+	// char buffer[ASSERTION_MESSAGE_BUFFER_SIZE];
 
-	va_list args;
-	va_start(args, format);
-	portLibrary->str_vprintf(portLibrary, buffer, ASSERTION_MESSAGE_BUFFER_SIZE, format, args);
-	va_end(args);
+	// va_list args;
+	// va_start(args, format);
+	// portLibrary->str_vprintf(portLibrary, buffer, ASSERTION_MESSAGE_BUFFER_SIZE, format, args);
+	// va_end(args);
 
-	if (NULL != omrVMThread) {
-		/* omrVMThread might be NULL if Environment is partially initialized at startup time */
-		Trc_MM_AssertionWithMessage_outputMessage(omrVMThread->_language_vmthread, buffer);
-	}
-	portLibrary->tty_printf(portLibrary, "%s", buffer);
+	// if (NULL != omrVMThread) {
+	// 	/* omrVMThread might be NULL if Environment is partially initialized at startup time */
+	// 	Trc_MM_AssertionWithMessage_outputMessage(omrVMThread->_language_vmthread, buffer);
+	// }
+	// portLibrary->tty_printf(portLibrary, "%s", buffer);
+
+
+	printf(">>> EHSAN: %s\n",format);
 }
 
 #endif /* defined(OMR_GC_DEBUG_ASSERTS) */
