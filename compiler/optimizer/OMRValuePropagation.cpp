4d64486fb0a3f00bd5f073a19cbc358f882ddc38
Ehsan Kiani Far <ehsan.kianifar@gmail.com>
2025-01-20 10:00:30 -0500

    Revert "No constFolding"
    This reverts commit e6a830304b53b8569153bd86d602fb1254b8de05.

--- compiler/optimizer/OMRValuePropagation.cpp
+++ compiler/optimizer/OMRValuePropagation.cpp
@@ -2900,7 +2900,6 @@ TR::VPConstraint *OMR::ValuePropagation::mergeDefConstraints(TR::Node *node, int
 
 void OMR::ValuePropagation::replaceByConstant(TR::Node *node, TR::VPConstraint *constraint, bool isGlobal)
    {
-      return;
    if (isGlobal)
       addGlobalConstraint(node, constraint);
    else
