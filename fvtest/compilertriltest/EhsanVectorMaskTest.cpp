26dd1a289f3326135c61ef4b34158e8490d669db
Ehsan Kiani Far <ehsan.kianifar@gmail.com>
2025-09-10 15:59:52 -0400

    Fix
    Fix

--- fvtest/compilertriltest/EhsanVectorMaskTest.cpp
+++ fvtest/compilertriltest/EhsanVectorMaskTest.cpp
@@ -134,7 +134,7 @@ TEST_P(ParameterizedUnaryMaskTest, loadConst) {
    {
       value[valueIndex] = '0';
       valueIndex++;
-      value[valueIndex] = (char)((unit8_t)'0'+input[i]);
+      value[valueIndex] = (char)((uint8_t)'0'+input[i]);
       valueIndex++;
    }
 
