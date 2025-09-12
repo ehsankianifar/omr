4d3f3129ffa807c3ecbb4a0b9048ac85c9291329
Ehsan Kiani Far <ehsan.kianifar@gmail.com>
2025-09-12 10:47:55 -0400

    Add vectotTest
    A

--- fvtest/compilertriltest/EhsanVectorMaskTest.cpp
+++ fvtest/compilertriltest/EhsanVectorMaskTest.cpp
@@ -28,6 +28,7 @@
 class VectorTest : public TRTest::JitTest {};
 
 class ParameterizedUnaryMaskTest : public VectorTest, public ::testing::WithParamInterface<std::tuple<const char *, const char *, const char *, const char *>> {};
+class ParameterizedUnaryMaskTest2 : public VectorTest, public ::testing::WithParamInterface<std::tuple<const char *, const char *, const char *>> {};
 
 
 uint8_t *convertResultCharToArray(const char *input) {
@@ -48,8 +49,8 @@ uint8_t *convertResultCharToArray(const char *input) {
 }
 
 uint8_t *convertInputCharToArray(const char *input) {
-   uint8_t *result =(uint8_t*)malloc(8);
-   memset((void*)result, 0, 8);
+   uint8_t *result =(uint8_t*)malloc(16);
+   memset((void*)result, 0, 16);
    int index = 0;
    for (int i=0; input[i]!='\0'; i++) {
       if (input[i]=='0') {
@@ -174,23 +175,86 @@ TEST_P(ParameterizedUnaryMaskTest, loadConst) {
     free(input);
     free(expectedOutput);
 }
-
-INSTANTIATE_TEST_CASE_P(indirect, ParameterizedUnaryMaskTest, ::testing::ValuesIn(*TRTest::MakeVector<std::tuple<const char *, const char *, const char *, const char *>>(
+// The last string in parameters is expected rewsult and each number is a byte which can be 0 (all bits are zero) or 1 (all bits are 1 in that byte)
+// The second last paratemet is the input wich is a boolean 0 or 1 in a byte!
+// The non number chars are just for readability and get ignored!
+INSTANTIATE_TEST_CASE_P(b2m, ParameterizedUnaryMaskTest, ::testing::ValuesIn(*TRTest::MakeVector<std::tuple<const char *, const char *, const char *, const char *>>(
    std::make_tuple("b", "Vector128Int64" , "0", "00000000_00000000"),
-   std::make_tuple("b", "Vector128Int64" , "1", "11111111_11111111"),
-   std::make_tuple("s", "Vector128Int64" , "11", "11111111_11111111"),
+   std::make_tuple("b", "Vector128Int64" , "1", "11111111_11111111")
+)));
+INSTANTIATE_TEST_CASE_P(s2m, ParameterizedUnaryMaskTest, ::testing::ValuesIn(*TRTest::MakeVector<std::tuple<const char *, const char *, const char *, const char *>>(
+   std::make_tuple("s", "Vector128Int64" , "00", "00000000_00000000"),
    std::make_tuple("s", "Vector128Int64" , "01", "00000000_11111111"),
    std::make_tuple("s", "Vector128Int64" , "10", "11111111_00000000"),
-   std::make_tuple("s", "Vector128Int64" , "11", "11111111_11111111"),
+   std::make_tuple("s", "Vector128Int64" , "11", "11111111_11111111")
+)));
+INSTANTIATE_TEST_CASE_P(i2m, ParameterizedUnaryMaskTest, ::testing::ValuesIn(*TRTest::MakeVector<std::tuple<const char *, const char *, const char *, const char *>>(
    std::make_tuple("i", "Vector128Int32" , "0111", "0000_1111_1111_1111"),
    std::make_tuple("i", "Vector128Int32" , "1011", "1111_0000_1111_1111"),
    std::make_tuple("i", "Vector128Int32" , "1101", "1111_1111_0000_1111"),
    std::make_tuple("i", "Vector128Int32" , "1110", "1111_1111_1111_0000"),
    std::make_tuple("i", "Vector128Int32" , "0000", "0000_0000_0000_0000"),
-   std::make_tuple("i", "Vector128Int32" , "1111", "1111_1111_1111_1111"),
+   std::make_tuple("i", "Vector128Int32" , "1111", "1111_1111_1111_1111")
+)));
+INSTANTIATE_TEST_CASE_P(l2m, ParameterizedUnaryMaskTest, ::testing::ValuesIn(*TRTest::MakeVector<std::tuple<const char *, const char *, const char *, const char *>>(
    std::make_tuple("l", "Vector128Int16" , "0000_0000", "00_00_00_00_00_00_00_00"),
    std::make_tuple("l", "Vector128Int16" , "1111_1111", "11_11_11_11_11_11_11_11"),
    std::make_tuple("l", "Vector128Int16" , "0111_1111", "00_11_11_11_11_11_11_11"),
    std::make_tuple("l", "Vector128Int16" , "1111_1110", "11_11_11_11_11_11_11_00"),
    std::make_tuple("l", "Vector128Int16" , "1010_1010", "11_00_11_00_11_00_11_00")
+)));
+
+
+TEST_P(ParameterizedUnaryMaskTest2, loadVector) {
+   const char *size = std::get<0>(GetParam());
+   const char *typeString = std::get<1>(GetParam());
+   const char *inputChar = std::get<2>(GetParam());
+   const char *resultChar = std::get<3>(GetParam());
+
+   char inputTrees[1024];
+   char *formatStr = "(method return= NoType args=[Address,Address]                   "
+                     "  (block                                                        "
+                     "     (vstoreiVector128Int8 offset=0                             "
+                     "         (aload parm=0)                                         "
+                     "         (v2m%s                                                "
+                     "              (vloadi                                          "
+                     "                 (aload parm=1))))                              "
+                     "     (return)))                                                 ";
+
+   sprintf(inputTrees, formatStr, typeString);
+    auto trees = parseString(inputTrees);
+
+    ASSERT_NOTNULL(trees);
+
+    Tril::DefaultCompiler compiler(trees);
+    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
+
+    auto entry_point = compiler.getEntryPoint<void (*)(uint8_t*,uint8_t*)>();
+
+    uint8_t output[] =  {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+    uint8_t *inputA =  convertInputCharToArray(inputChar);
+    uint8_t *expectedOutput = convertResultCharToArray(resultChar);
+
+    entry_point(output,inputA);
+
+    for (int i = 0; i < 16; i++) {
+        EXPECT_EQ(expectedOutput[i], output[i]);
+    }
+    free(inputA);
+    free(expectedOutput);
+}
+
+
+INSTANTIATE_TEST_CASE_P(v2m, ParameterizedUnaryMaskTest, ::testing::ValuesIn(*TRTest::MakeVector<std::tuple<const char *, const char *, const char *, const char *>>(
+   std::make_tuple("Vector128Int8" , "0000000000000000", "0000000000000000"),
+   std::make_tuple("Vector128Int8" , "0000000000000001", "0000000000000001"),
+   std::make_tuple("Vector128Int8" , "0000000000000010", "0000000000000010"),
+   std::make_tuple("Vector128Int8" , "1110000000000000", "1110000000000000"),
+   std::make_tuple("Vector128Int8" , "0000000011111111", "0000000011111111"),
+   std::make_tuple("Vector128Int8" , "1111111111111111", "1111111111111111"),
+   // Only Vector128Int8 is planned but lets test one other type since we support it!
+   std::make_tuple("Vector128Int64" , "00000000_00000000", "00000000_00000000"),
+   std::make_tuple("Vector128Int64" , "00000000_00000001", "00000000_11111111"),
+   std::make_tuple("Vector128Int64" , "00000001_00000000", "11111111_00000000"),
+   std::make_tuple("Vector128Int64" , "00000001_00000001", "11111111_11111111")
 )));
