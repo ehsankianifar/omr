49e1a330cf624a4b22648c788998cf539c2fba74
ehsankianifar <ehsan.kianifar@gmail.com>
2026-01-22 15:19:32 -0500

    fix

--- compiler/codegen/CodeGenRA.cpp
+++ compiler/codegen/CodeGenRA.cpp
@@ -994,8 +994,8 @@ TR_GlobalRegisterNumber OMR::CodeGenerator::pickRegister(TR::RegisterCandidate *
 
     TR::Compilation *comp = self()->comp();
     OMR::Logger *log = comp->log();
-    bool traceSimulateTreeEvaluation = true;//self()->traceSimulateTreeEvaluation();
-    bool terseSimulateTreeEvaluation = true;//self()->terseSimulateTreeEvaluation();
+    bool traceSimulateTreeEvaluation = log != NULL;//self()->traceSimulateTreeEvaluation();
+    bool terseSimulateTreeEvaluation = log != NULL;//self()->terseSimulateTreeEvaluation();
     bool traceGRA = comp->getOptions()->trace(OMR::tacticalGlobalRegisterAllocator);
 
     if (!isInitialized) {
